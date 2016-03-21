502d6816109bf7429280150c98c7229d56881afc
vilmesh <vitalii.meshko@gmail.com>
2016-03-21 16:21:56 +0000

    add auth

---
+++ client/src/chess/auth/index.js
@@ -0,0 +1,67 @@
+var angular = require('angular');
+var ngRoute = require('angular-route');
+
+module.exports = angular.module('chess.auth', [
+  ngRoute
+]).
+controller('formAction', ['$scope', '$location', '$http', function($scope, $location, $http) {
+	$scope.submitLoginForm = function () {
+    var data = {
+      username : $scope.fields.username,
+      password : $scope.fields.password
+    };
+    /* post to server*/
+    var url = '/login';
+    $http.post(url, data)
+    .success(function (data, status, headers, config) {
+      $location.path( "/chess" );
+      alert(status+": "+ data.message);
+    })
+    .error(function (data, status, headers, config) {
+      alert(status+": "+ data.message);
+    });
+  }
+
+  $scope.register = function() {
+    var data = {
+      username : $scope.fields.username,
+      password : $scope.fields.password
+    };
+    /* post to server*/
+    var url = '/register';
+    $http.post(url, data)
+    .success(function (data, status, headers, config) {
+      alert(status+": "+ data.message);
+    })
+    .error(function (data, status, headers, config) {
+      alert(status+": "+ data.message);
+    });
+  }
+}]).
+controller('chessCntr', ['$scope', '$location', '$http', function($scope, $location, $http) {
+  $http.get('/chess')
+    .success(function (data, status, headers, config) {
+      $scope.username = data.username;
+    })
+    .error(function (data, status, headers, config) {
+      $location.path("/login");
+    });
+  $scope.logout = function(){
+    $http.post('/logout')
+      .success(function (data, status, headers, config){
+         $location.path("/");
+      })
+      .error(function (data, status, headers, config) {
+        console.log(data);
+      });
+  }
+}]).
+config(RouteConfig);
+
+RouteConfig.$inject = ['$routeProvider'];
+function RouteConfig($routeProvider) {
+  $routeProvider.when('/login', {
+  		templateUrl: 'auth/login.html',
+  		controller : 'formAction'
+  	});
+}
