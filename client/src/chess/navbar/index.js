fd7b91b873aa505424957b436ff074bea72fe75c
Volodymyr Matyukh <matyukh.volodymyr@gmail.com>
2016-04-01 17:25:19 +0300

    create CheckAuth for Game page

--- client/src/chess/navbar/index.js
+++ client/src/chess/navbar/index.js
@@ -1,10 +1,30 @@
 var angular = require('angular');
+var auth = require('../auth');
 
-module.exports = angular.module('chess.navbar', []).
+module.exports = angular.module('chess.navbar', [
+  auth.name
+]).
 component('navbar', {
   controller: NavbarController,
   templateUrl: 'navbar/navbar.component.html'
 });
 
-NavbarController.$inject = []
-function NavbarController() {}
+NavbarController.$inject = ['auth', '$location'];
+function NavbarController(auth, $location) {
+  var $ctrl = this;
+
+  $ctrl.logout = logout;
+
+  function logout() {
+    auth.logout().
+      then(
+        function (response) {
+          $location.path('/')
+          console.log('user logged out');
+        },
+        function () {
+          console.log('error during logging out');
+        }
+      );
+  }
+}
