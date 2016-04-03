41f1594d453eaad116c9f6c167abe12abdf1f34b
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-03 23:08:25 +0300

    Split Http server, Express app and SocketIo into seperate modules. Add basic skeleton for sockets.

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,7 @@
 var io = require('socket.io-client');
 var chess = require('./chess');
+var socket = io(process.env.API_URL);
+
+socket.on('connect', function () {
+  console.log('connected!');
+});
