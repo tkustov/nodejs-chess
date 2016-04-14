67151e43a59cb053da4b13601b009905dd786571
anastasiagoncharova <goncharova_ana@ukr.net>
2016-04-14 14:00:08 +0000

    change settings page

--- client/src/chess/settings/index.js
+++ client/src/chess/settings/index.js
@@ -19,16 +19,20 @@ function SettingsController(){
   ctrl.photobutton.addEventListener("click", photoShow);
   function photoShow(){
     ctrl.photodiv = document.getElementById("photo");
-    if(ctrl.photodiv.style.display = "none"){
+    if(ctrl.photodiv.style.display === "none"){
       ctrl.photodiv.style.display = "block";
+    }else{
+      ctrl.photodiv.style.display = "none";
     }
 }
 ctrl.passbutton = document.getElementById("passbutton");
   ctrl.passbutton.addEventListener("click", passShow);
   function passShow(){
-    ctrl.passdiv = document.getElementById("pass");
-    if(ctrl.passdiv.style.display = "none"){
+    ctrl.passdiv = document.getElementById("password_modal");
+    if(ctrl.passdiv.style.display === "none"){
       ctrl.passdiv.style.display = "block";
+    }else{
+      ctrl.passdiv.style.display = "none";
     }
-}
+  }
 }
