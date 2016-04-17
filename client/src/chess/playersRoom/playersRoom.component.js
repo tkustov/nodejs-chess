368e66f2c93969489d46814198ff6dc5bdc77ee3
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-04-17 15:15:27 +0300

    add user to userRoom on socket connect

--- client/src/chess/playersRoom/playersRoom.component.js
+++ client/src/chess/playersRoom/playersRoom.component.js
@@ -11,10 +11,4 @@ function PlayersRoomController(PlayersRoom, Socket, $http, $location) {
   $ctrl.incommingInvites = [];
   $ctrl.status = "Can't connect to Socket! Server is not running or your internet connection is bad :(";
 
-  $http.get(process.env.API_URL + '/api/user/room/', {withCredentials: true})
-  .then(function(response) {
-    var userRoom = response.data.userRoom.toString();
-    console.log('user room: ', userRoom);
-  });
-  
 };
