464b9548a8a56bca3c78f2631acc95506e65a77f
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-04-17 13:13:32 +0300

    Remove unnecessary code from playersRoom controller. Get from server and print to console userRoom.

--- client/src/chess/playersRoom/playersRoom.component.js
+++ client/src/chess/playersRoom/playersRoom.component.js
@@ -9,6 +9,12 @@ function PlayersRoomController(PlayersRoom, Socket, $http, $location) {
 
   $ctrl.usersOnline = [];
   $ctrl.incommingInvites = [];
-  $ctrl.status = "Can not connect ot Socket! Server is not running or your internet connection is bad :(";
+  $ctrl.status = "Can't connect to Socket! Server is not running or your internet connection is bad :(";
 
+  $http.get(process.env.API_URL + '/api/user/room/', {withCredentials: true})
+  .then(function(response) {
+    var userRoom = response.data.userRoom.toString();
+    console.log('user room: ', userRoom);
+  });
+  
 };
