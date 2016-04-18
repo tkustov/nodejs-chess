9d052d3712dd65594c074f169711a38ed659ec56
Elizabeth Fomenko <Elizabeth Fomenko>
2016-04-18 23:51:00 +0300

    adding chat

--- client/src/chess/chat/index.js
+++ client/src/chess/chat/index.js
@@ -2,13 +2,21 @@ var angular = require('angular');
 var ngRoute = require('angular-route');
 
 module.exports = angular.module('chess.chat', [
-  ngRoute
+  //ngRoute
 ]).
+//config(RouteConfig).
 component('chat', {
   controller: ChatController,
   templateUrl: 'chat/chat.component.html'
 });
+// RouteConfig.$inject = ['$routeProvider'];
+// function RouteConfig($routeProvider) {
+//   $routeProvider.when('/board', {
+//     templateUrl: 'chat/chat.component.html'
+//   });
+// }
 
 function ChatController(){
-	
+	console.log('Chat Controller');
+
 }
