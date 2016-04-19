fdc431ffc44d36d8523353f643230d3830aedbe4
Elizabeth Fomenko <Elizabeth Fomenko>
2016-04-19 09:17:39 +0300

    adding chat

--- client/src/chess/chat/index.js
+++ client/src/chess/chat/index.js
@@ -1,22 +1,58 @@
 var angular = require('angular');
 var ngRoute = require('angular-route');
+var SocketFactory = require('../socket/socket.factory');
 
 module.exports = angular.module('chess.chat', [
-  //ngRoute
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io'
 ]).
+.value('nickName', 'anonymous');
 //config(RouteConfig).
 component('chat', {
   controller: ChatController,
   templateUrl: 'chat/chat.component.html'
 });
-// RouteConfig.$inject = ['$routeProvider'];
-// function RouteConfig($routeProvider) {
-//   $routeProvider.when('/board', {
-//     templateUrl: 'chat/chat.component.html'
-//   });
-// }
 
-function ChatController(){
+function ChatController(socket){
 	console.log('Chat Controller');
+	
+	socket.on('init', function (data) {
+    $scope.name = data.name;
+    $scope.users = data.users;
+  });
 
+  socket.on('send:message', function (message) {
+    $scope.messages.push(message);
+  });
+
+  socket.on('user:join', function (data) {
+    $scope.messages.push({
+      user: 'chatroom',
+      text: 'User ' + data.name + ' has joined.'
+    });
+    $scope.users.push(data.name);
+  });
+
+    $scope.messages.push({
+      user: 'chatroom',
+      text: 'User ' + oldName + ' is now known as ' + newName + '.'
+    });
+  }
+
+  $scope.sendMessage = function () {
+    socket.emit('send:message', {
+      message: $scope.message
+    });
+
+    // add the message to our model locally
+    $scope.messages.push({
+      user: $scope.name,
+      text: $scope.message
+    });
+
+    // clear message box
+    $scope.message = '';
+  };
 }
