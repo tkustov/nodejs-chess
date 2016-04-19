23ef6de455729a72f4489c3835ef209da067150b
Elizabeth Fomenko <Elizabeth Fomenko>
2016-04-19 12:40:47 +0300

    delete chat controller

--- client/src/chess/chat/index.js
+++ client/src/chess/chat/index.js
@@ -3,56 +3,15 @@ var ngRoute = require('angular-route');
 var SocketFactory = require('../socket/socket.factory');
 
 module.exports = angular.module('chess.chat', [
- /* 'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'btford.socket-io'*/
 ]).
-//.value('nickName', 'anonymous');
-//config(RouteConfig).
+
 component('chat', {
   controller: ChatController,
   templateUrl: 'chat/chat.component.html'
 });
 
-function ChatController(socket){
+function ChatController(){
 	console.log('Chat Controller');
-	
-	/*socket.on('init', function (data) {
-    $scope.name = data.name;
-    $scope.users = data.users;
-  });
-
-  socket.on('send:message', function (message) {
-    $scope.messages.push(message);
-  });
-
-  socket.on('user:join', function (data) {
-    $scope.messages.push({
-      user: 'chatroom',
-      text: 'User ' + data.name + ' has joined.'
-    });
-    $scope.users.push(data.name);
-  });
-
-    $scope.messages.push({
-      user: 'chatroom',
-      text: 'User ' + oldName + ' is now known as ' + newName + '.'
-    });
-  }
-
-  $scope.sendMessage = function () {
-    socket.emit('send:message', {
-      message: $scope.message
-    });
-
-    // add the message to our model locally
-    $scope.messages.push({
-      user: $scope.name,
-      text: $scope.message
-    });
-
-    // clear message box
-    $scope.message = '';
-  };*/
+	var $ctrl=this;
+	$ctrl.name="Hello";	
 }
