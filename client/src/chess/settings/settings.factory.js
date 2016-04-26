5aad6e60388e6e1c1bd0b7211282b621640f805c
Volodymyr Matyukh <matyukh.volodymyr@gmail.com>
2016-04-26 18:52:54 +0300

    create method deleteUserAccount in user module

--- client/src/chess/settings/settings.factory.js
+++ client/src/chess/settings/settings.factory.js
@@ -1,27 +1,28 @@
 module.exports = SettingsFactory;
 
-SettingsFactory.$inject = ['$http'];
-function SettingsFactory($http) {
+SettingsFactory.$inject = ['$http', '$rootScope'];
+function SettingsFactory($http, $rootScope) {
   return {
     changePassword: changePassword,
     deleteAccount: deleteAccount
   };
-  
+
   function changePassword(data) {
      return $http.put(process.env.API_URL + '/api/user/password', data, {withCredentials: true})
       .then(
         function(response) {
           return response.status + ' ' + response.statusText;
-        }  
+        }
       );
   }
-  
+
   function deleteAccount() {
+    $rootScope.$broadcast('socketDisconnect');
     return $http.delete(process.env.API_URL + '/api/user/account', {withCredentials: true})
       .then(
         function(response) {
           return response.status + ' ' + response.statusText;
-        }  
+        }
       );
   }
 }
