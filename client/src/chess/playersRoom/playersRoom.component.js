d08e5a455f19a422c81057ef7e0c6c4d6d04c0dd
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-04-26 15:55:42 +0300

    client side skeleton for invitations system

--- client/src/chess/playersRoom/playersRoom.component.js
+++ client/src/chess/playersRoom/playersRoom.component.js
@@ -15,8 +15,35 @@ function PlayersRoomController(PlayersRoom, Socket, $http, $location, $scope, us
     $http.get(process.env.API_URL + '/api/user/users-online/', {withCredentials: true})
     .then(function(response) {
       $ctrl.usersOnline = response.data;
+      console.log( $ctrl.usersOnline);
     });
   };
   $ctrl.getUsersOnline();
-  
+
+  var gameSocket = Socket('game');
+
+  gameSocket.on('userJoined', function(data){
+    if (user.userInfo._id != data.id) {$ctrl.usersOnline.push(data)};
+  });
+
+  $ctrl.sendInvitation = function(userId){
+    $http.get(process.env.API_URL + '/api/game/invitation/send/'+userId, {withCredentials: true})
+    .then(function(response) {
+      console.log('send invitation');
+    });
+  };
+
+  $ctrl.acceptInvitation = function (userId) {
+    $http.get(process.env.API_URL + '/api/game/invitation/accept/'+userId, {withCredentials: true})
+    .then(function(response) {
+      console.log('accept invitation');
+    });
+  };
+
+  gameSocket.on('startGame', function(data){
+    //set game id
+    //redirect to game path
+  });
+
+
 }
