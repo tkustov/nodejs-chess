7c414f3506971448c06b05ec1c543bd9ce7f0eeb
vilmesh <vitalii.meshko@gmail.com>
2016-04-29 13:39:06 +0000

    refactored settings component code

--- client/src/chess/user/index.js
+++ client/src/chess/user/index.js
@@ -1,9 +1,9 @@
 module.exports = 'chess.user';
 
-angular.module('chess.user', [])
-.factory('user', UserFactory);
+angular.module('chess.user', []).
+factory('user', UserFactory);
 
-UserFactory.$inject = ['$http','$rootScope']
+UserFactory.$inject = ['$http','$rootScope'];
 function UserFactory($http, $rootScope) {
 
   var factory = {
@@ -20,14 +20,10 @@ function UserFactory($http, $rootScope) {
 
   function getUserInfo() {
     return $http.get(process.env.API_URL + '/api/user/info', {withCredentials: true}).
-      then(
-        function(response) {
-          factory.userInfo = response.data;
-          $rootScope.$broadcast('Authorized');
-        },
-        function(response) {
-          factory.userInfo = null;
-        });
+      then(function(response) {
+        factory.userInfo = response.data;
+        $rootScope.$broadcast('Authorized');
+      });
   }
 
   function isLoggedIn() {
