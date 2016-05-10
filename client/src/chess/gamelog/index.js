271d66d206769f20804612e4dd354d946d75260a
BLukash <bohdan.lukashchuk@gmail.com>
2016-05-10 10:50:44 +0300

    added scatch of gamelog controller

--- client/src/chess/gamelog/index.js
+++ client/src/chess/gamelog/index.js
@@ -1,6 +1,5 @@
 var angular = require('angular');
 var ngRoute = require('angular-route');
-var SocketFactory = require('../socket/socket.factory');
 var user = require('../user');
 var GameFactory = require('../game/game.factory');
 
@@ -9,53 +8,22 @@ module.exports = angular.module('chess.gamelog', [
 ]).
 factory('Game', GameFactory).
 component('gamelog', {
-  controller: GameLogController,
+  controller: GamelogController,
   templateUrl: 'gamelog/gamelog.component.html'
 });
 
-GameLogController.$inject = ['Socket', '$http', 'user', 'Game'];
-function GameLogController(Socket, $http, user, Game){
+GamelogController.$inject = ['user', 'Game'];
+function GamelogController(user, Game) {
 	var $ctrl = this;
 	$ctrl.moves = [];
-	//var elem = document.getElementById('chatbody');
+  $scope.$watch(Game.getFactoryMoves, function (moves) {
+    ctrl.moves = moves;
+    console.log(ctrl.moves + ' мувси для геймлогу');
+  }, true);
 
 	$ctrl.getUserName = function() {
 		    return (user.userInfo)
 		      ? user.userInfo.username
 		      : null;
 	};
-
-	//var gameId=Game.getGameId();
-
-	// $http.get(process.env.API_URL + '/api/game/gameroom'+ gameId, {withCredentials: true})
-  //   .then(function(response) {
-  //   	var gameRoom = response.data["game room"];
-	//     chatSocket = Socket('chat');
-  //
-	//     chatSocket.on('connect', function(data) {
-	//       chatSocket.emit('join', gameRoom);
-	//     });
-  //
-	// 	chatSocket.on('send:message', function (message) {
-	// 	    $ctrl.messages.push(message);
-	// 	    $ctrl.gotoBottom();
-	// 	});
-
-	// 	$ctrl.gotoBottom = function() {
-	// 		elem.scrollTop = elem.scrollHeight - elem.clientHeight;
-	// 	};
-  //
-	// 	$ctrl.sendMessage = function () {
-	// 		var sender=$ctrl.getUserName();
-	// 	    chatSocket.emit('send:message', {
-	// 	    	who: sender,
-	// 	      	message: $ctrl.message
-	// 		});
-	// 	    $ctrl.message = '';
-	// 	};
-	// 	$rootScope.$on('disconnectGameSocket', function(){
-	//     	chatSocket.disconnect();
-	//     	console.log('disconnected from "game" namespace');
-  // 		});
-	// });
 }
