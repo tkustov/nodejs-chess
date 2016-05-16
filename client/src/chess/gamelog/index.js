b6d47522936600bd3785549392f42814ae5bbda4
BLukash <bohdan.lukashchuk@gmail.com>
2016-05-16 12:39:01 +0300

    fixed gamelog

--- client/src/chess/gamelog/index.js
+++ client/src/chess/gamelog/index.js
@@ -15,14 +15,95 @@ component('gamelog', {
   templateUrl: 'gamelog/gamelog.component.html'
 });
 
-GamelogController.$inject = ['user', 'Game', '$scope'];
-function GamelogController(user, Game, $scope) {
+GamelogController.$inject = ['user', 'Game', '$scope', '$q'];
+function GamelogController(user, Game, $scope, $q) {
 	var $ctrl = this;
   $ctrl.moves = [];
-  var gameInfo = Game.getGameInfo()
+  $ctrl.color;
+  $ctrl.whoMoves;
+  var userInfo;
+  var tmpMoves;
+  $scope.$watch(Game.getFactoryMoves, function (moves, movesOld) {
+    getUserInfo().then(function(prom) {
+      fillMoves(moves, movesOld, prom);
+    });
 
-  $scope.$watch(Game.getFactoryMoves, function (moves) {
-    $ctrl.moves = moves;
   }, true);
 
+  // $scope.$watch(Game.getGameInfo, function (info) {
+  //   if (userInfo._id === info.whitePlayer) {
+  //     $ctrl.whoMoves = 'Yours';
+  //     $ctrl.color = 'WHITE';
+  //   }
+  //   else if ((userInfo._id === info.blackPlayer)) {
+  //     $ctrl.whoMoves = 'Opponents';
+  //     $ctrl.color = 'BLACK';
+  //   }
+  // })
+
+  function findColor () {
+    var info = Game.getGameInfo();
+      if (userInfo._id === info.whitePlayer) {
+        $ctrl.whoMoves = 'Yours';
+        $ctrl.color = 'WHITE';
+      }
+      else if ((userInfo._id === info.blackPlayer)) {
+        $ctrl.whoMoves = 'Opponents';
+        $ctrl.color = 'BLACK';
+      }
+  }
+
+  function fillMoves (moves, movesOld, prom) {
+    userInfo = prom;
+    findColor();
+    if (((moves.length - movesOld.length) > 1) && movesOld.length !== 0) {
+      moves.pop();
+    }
+    if (moves.length === 0) {
+      moves = Game.getFactoryMoves();
+    }
+    tmpMoves =  moves.map(function(item, i) {
+      if (userInfo._id === Game.whitePlayer) {
+        if (i === 0) {
+          item.user = 'You';
+          $ctrl.whoMoves = 'Opponents';
+        }
+        if (i & 1) {
+          item.user = 'Opponent';
+          $ctrl.whoMoves = 'Yours';
+        }
+        else {
+          item.user = 'You';
+          $ctrl.whoMoves = 'Opponents';
+        }
+      }
+      else {
+        if (i & 1) {
+          item.user = 'You';
+          $ctrl.whoMoves = 'Opponents';
+        }
+        else {
+          item.user = 'Opponent';
+          $ctrl.whoMoves = 'Yours';
+        }
+      }
+      return item;
+    });
+    $ctrl.moves = tmpMoves;
+  }
+
+  function getUserInfo () {
+    var promise = $q(function (resolve, reject) {
+      if (user.userInfo === null) {
+        user.getUserInfo().
+          then(function () {
+            resolve(user.userInfo);
+          });
+        }
+        else {
+          resolve(user.userInfo);
+        }
+    });
+    return promise;
+  }
 }
